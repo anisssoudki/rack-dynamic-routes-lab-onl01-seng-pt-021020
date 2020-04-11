d13ff0acb040a252a4bee7d9f2aa526b7b9ee558
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-11 04:53:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,9 @@ class Application
            resp.status = 404
          end
       else
-        resp.write "Route not found"
+        
         resp.status = 404
+        resp.write "Route not found"
        end
     resp.finish
   end
